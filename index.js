a2bcdbd9113f17cae8a991d15754c9ec51be8cc7
Anam I <anami@Anams-MacBook-Air.local>
2021-03-09 21:34:21 -0500

    hw part 2

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function myFunction(){
+function myFunction() {
     var x = document.getElementById("box1").value
     var y = document.getElementById("box2").value
-    
-    var z = Number(x)+Number(y)
 
-    document.getElementById("box3").value = z
-    
+    var operator = document.getElementById("values").value
+    var result = 0
+
+    if (operator == "add") {
+        result = Number(x) + Number(y)
+        document.getElementById("box3").value = result
+    } else if (operator == "sub") {
+        result = Number(x) - Number(y)
+        document.getElementById("box3").value = result
+    } else if (operator == "div") {
+        result = Number(x) / Number(y)
+        document.getElementById("box3").value = result
+    } else if (operator == "mul") {
+        result = Number(x) * Number(y)
+        document.getElementById("box3").value = result
+
+
+    }
+
+
 }
-    
